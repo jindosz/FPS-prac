e7541925169ec5a9f20a67a0f49c2a3a852bfa2f
jindosz <127918186+jindosz@users.noreply.github.com>
2023-05-29 01:59:09 +0900

    #2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private RotateToMouse  rotateToMouse;
+    [Header("Input KeyCodes")]
+    [SerializeField]
+    private KeyCode     keyCodeRun = KeyCode.LeftShift;
+    [SerializeField]
+    private KeyCode     keyCodeJump = KeyCode.Space;
+
+    [Header("Audio Clips")]
+    [SerializeField]
+    private AudioClip   audioClipWalk;
+    [SerializeField]
+    private AudioClip   audioClipRun;
+
+    private RotateToMouse               rotateToMouse;
+    private MovementCharacterController movement;
+    private Staturs                     staturs;
+    private PlayerAnimatorController    animator;
+    private AudioSource                 audioSource;
 
     private void Awake()
     {
@@ -12,11 +28,17 @@ public class PlayerController : MonoBehaviour
         Cursor.lockState    = CursorLockMode.Locked;
 
         rotateToMouse       = GetComponent<RotateToMouse>();
+        movement            = GetComponent<MovementCharacterController>();
+        staturs             = GetComponent<Staturs>();
+        animator            = GetComponent<PlayerAnimatorController>();
+        audioSource         = GetComponent<AudioSource>();
     }
 
     private void Update()
     {
         UpdateRotate();
+        UpdateMove();
+        UpdateJump();
     }
 
     private void UpdateRotate()
@@ -25,5 +47,49 @@ public class PlayerController : MonoBehaviour
         float mouseY = Input.GetAxis("Mouse Y");
 
         rotateToMouse.UpdateRotate(mouseX, mouseY);
+    }
+
+    private void UpdateMove()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        if ( x  != 0 || z != 0)
+        {
+            bool isRun = false;
+
+            if ( z > 0 ) isRun = Input.GetKey(keyCodeRun);
+
+            movement.MoveSpeed = isRun == true ? staturs.RunSpeed : staturs.WalkSpeed;
+            animator.MoveSpeed = isRun == true ? 1 : 0.5f;
+            audioSource.clip   = isRun == true ? audioClipRun : audioClipWalk;
+        
+            if ( audioSource.isPlaying == false )
+            {
+                audioSource.loop = true;
+                audioSource.Play();
+            }
+        }
+
+        else
+        {
+            movement.MoveSpeed = 0;
+            animator.MoveSpeed = 0;
+
+            if ( audioSource.isPlaying == true )
+            {
+                audioSource.Stop();
+            }
+        }
+
+        movement.MoveTo(new Vector3(x, 0, z));
+    }
+
+    private void UpdateJump()
+    {
+        if ( Input.GetKeyDown(keyCodeJump) )
+        {
+            movement.Jump();
+        }
     }
 }
